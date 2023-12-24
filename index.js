873930fd45d3729d6e681ae7d869def7b3d9810f
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-24 09:18:08 +0100

    use notFound, errorHandler middlewares

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const config = require("./config");
+const notFound = require("./middlewares/noFound");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
 app.use("/api/v1/auth", require('./routes/auth.routes'));
+app.use(notFound);
+app.use(errorHandler);
 
 const port = config.APP_PORT
 
