020a98a7abdf7d93b4906cd8ccee464ea4f0b2b5
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-24 08:41:46 +0100

    add custom Error class that extend from Error class

---
+++ customError/index.js
@@ -0,0 +1,24 @@
+const { StatusCodes } = require("http-status-codes");
+
+class CustomError extends Error {
+  constructor(message, httpStatusCode) {
+    super(message);
+    this.httpStatusCode = httpStatusCode
+  }
+}
+
+module.exports.badRequestError = (message) => {
+  return new CustomError(message, StatusCodes.BAD_REQUEST)
+}
+
+module.exports.unAuthorizedError = (message) => {
+  return new CustomError(message, StatusCodes.UNAUTHORIZED)
+}
+
+module.exports.forbiddenError = (message) => {
+  return new CustomError(message, StatusCodes.FORBIDDEN)
+}
+
+module.exports.notFoundError = (message) => {
+  return new CustomError(message, StatusCodes.FORBIDDEN)
+}
